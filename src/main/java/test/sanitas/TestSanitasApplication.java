8fe6500ab79f5dad523db70478d9e07a9d71e2c1
Javier Martín Lozano <javier.martinlozano@servexternos.gruposantander.es>
2022-07-04 17:26:20 +0200

    Añadido comentario

--- src/main/java/test/sanitas/TestSanitasApplication.java
+++ src/main/java/test/sanitas/TestSanitasApplication.java
@@ -3,9 +3,17 @@ package test.sanitas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TestSanitasApplication.
+ *
+ */
 @SpringBootApplication
 public class TestSanitasApplication {
 
+	/**
+	 * Main que arranca el microservicio.
+	 * @param args the args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TestSanitasApplication.class, args);
 	}
