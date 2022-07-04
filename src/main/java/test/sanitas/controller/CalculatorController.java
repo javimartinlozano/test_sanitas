3c531680c56c90069ec4c0a91c6e526d3947d2b2
Javier Martín Lozano <javier.martinlozano@servexternos.gruposantander.es>
2022-07-04 16:33:50 +0200

    Llamada al service desde el controller

--- src/main/java/test/sanitas/controller/CalculatorController.java
+++ src/main/java/test/sanitas/controller/CalculatorController.java
@@ -5,11 +5,14 @@ package test.sanitas.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import test.sanitas.service.CalculatorService;
 
 /**
  * CalculatorController.
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
+
+	@Autowired
+	private CalculatorService calculatorService;
 
 	/**
 	 * Operación suma.
@@ -29,7 +35,7 @@ public class CalculatorController {
 	@GetMapping("/suma")
 	public ResponseEntity<BigDecimal> getSuma(@RequestParam(name = "valor1", required = true) BigDecimal valor1,
 			@RequestParam(name = "valor2", required = true) BigDecimal valor2) {
-		return ResponseEntity.ok(BigDecimal.ONE);
+		return ResponseEntity.ok(calculatorService.suma(valor1, valor2));
 	}
 
 	/**
@@ -42,6 +48,6 @@ public class CalculatorController {
 	@GetMapping("/resta")
 	public ResponseEntity<BigDecimal> getResta(@RequestParam(name = "valor1", required = true) BigDecimal valor1,
 			@RequestParam(name = "valor2", required = true) BigDecimal valor2) {
-		return ResponseEntity.ok(BigDecimal.ZERO);
+		return ResponseEntity.ok(calculatorService.resta(valor1, valor2));
 	}
 }
