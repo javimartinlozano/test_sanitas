fad6907fa7fb2b97e490a8ea43840841bd3b99dc
Javier Martín Lozano <javier.martinlozano@servexternos.gruposantander.es>
2022-07-04 16:02:38 +0200

    Crear estructura

---
+++ src/main/java/test/sanitas/TestSanitasApplication.java
@@ -0,0 +1,13 @@
+package test.sanitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSanitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSanitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/sanitas/TestSanitasApplicationTests.java
@@ -0,0 +1,13 @@
+package test.sanitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSanitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
